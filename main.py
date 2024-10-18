81a65b0c3cfc2bb4efbdaf49c5c737a662037b13
Samyak <samyak15.sj@gmail.com>
2024-10-18 20:12:11 +0530

    initial commit

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import os
 from telebot import TeleBot
 from handlers import register_handlers
 from config import TELEGRAM_BOT_TOKEN, UPLOAD_FOLDER
-from flask import Flask
-from threading import Thread
-
-app = Flask(__name__)
 
 # Ensure upload folder exists
 if not os.path.exists(UPLOAD_FOLDER):
@@ -17,18 +13,6 @@ bot = TeleBot(TELEGRAM_BOT_TOKEN)
 # Register all handlers
 register_handlers(bot)
 
-@app.route('/')
-def index():
-    return "Telegram Bot is Running"
-
-def run_bot():
+if __name__ == "__main__":
     print("Bot is running...")
     bot.polling(none_stop=True)
-
-if __name__ == "__main__":
-    # Start the bot in a new thread
-    Thread(target=run_bot).start()
-    
-    # Start the Flask app (Render requires this to keep the service running)
-    port = int(os.environ.get("PORT", 5001))  # Render sets this environment variable
-    app.run(host='0.0.0.0', port=port)
